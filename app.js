33ab55f5f8baacb3227275ca22bd753ae3b6a7a3
Wilson Tseng <p780729@hotmail.com>
2020-03-02 16:46:03 +0800

    install eslint and fix code style to Js Standard

--- app.js
+++ app.js
@@ -7,46 +7,43 @@ const isUserValid = require('./isUserValid')
 const session = require('express-session')
 
 app.use(session({
-    secret: 'Alpha camp Account_Login',
-    rolling: true,
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-        maxAge: 20* 1000
-    }
+  secret: 'Alpha camp Account_Login',
+  rolling: true,
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 20 * 1000
+  }
 }))
 
-//setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-//setting bodyParser
+// setting bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//setting router
+// setting router
 app.get('/', (req, res) => {
-    const sess = req.session
-    // console.log('session', sess)
-    if(sess.userName){
-        res.render('welcome', {userName: sess.userName})
-    }
-    else 
-        res.render('login')
+  const sess = req.session
+  // console.log('session', sess)
+  if (sess.userName) {
+    res.render('welcome', { userName: sess.userName })
+  } else res.render('login')
 })
 
 app.post('/', (req, res) => {
-    const user = req.body
-    if(isUserValid(user.email, user.password)){
-        req.session.regenerate(function(err) {
-            req.session.userName = isUserValid(user.email, user.password)
-        })
-    }
-    // console.log('Post reStore session:', req.session)
-    res.render('welcome', {userName: isUserValid(user.email, user.password)})
-    console.log('post')
+  const user = req.body
+  if (isUserValid(user.email, user.password)) {
+    req.session.regenerate(function () {
+      req.session.userName = isUserValid(user.email, user.password)
+    })
+  }
+  // console.log('Post reStore session:', req.session)
+  res.render('welcome', { userName: isUserValid(user.email, user.password) })
+  console.log('post')
 })
 
 app.listen(port, () => {
-    console.log('simple login server is running......')
+  console.log('simple login server is running......')
 })
-
